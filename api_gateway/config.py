f59ecd42d9cb28a01cb442aa3960445cc305c9d0
eskiturk <eskiturk2021@gmail.com>
2025-03-28 12:27:54 +0400

    1 jinb,rf ltgkjz

--- api_gateway/config.py
+++ api_gateway/config.py
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
     S3_BASE_PATH: str = os.getenv("S3_BASE_PATH", "user_data/")
 
     # CORS настройки - в продакшене указываем только нужные домены
-    CORS_ORIGINS: list = ["*"]
+    CORS_ORIGINS: list = os.getenv("CORS_ORIGINS", "*").split(",")
 
     # Настройки приложения
     APP_NAME: str = "Customer Management API"
